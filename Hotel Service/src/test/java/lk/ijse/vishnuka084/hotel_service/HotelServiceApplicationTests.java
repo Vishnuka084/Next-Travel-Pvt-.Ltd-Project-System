afbd997402e9e04cd0236f141832e516152ff234
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-30 13:04:42 +0530

    Add hotle service module

---
+++ Hotel Service/src/test/java/lk/ijse/vishnuka084/hotel_service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishnuka084.hotel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
