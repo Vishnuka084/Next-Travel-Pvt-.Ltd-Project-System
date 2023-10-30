cc3aa880b998de75ce25e422629be52ce2e71c08
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-30 12:49:32 +0530

    first commit

---
+++ Vehicale Service/src/main/java/lk/ijse/vishnuka084/vehicale_service/VehicaleServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishnuka084.vehicale_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicaleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicaleServiceApplication.class, args);
+    }
+
+}
---
+++ Vehicale Service/src/test/java/lk/ijse/vishnuka084/vehicale_service/VehicaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishnuka084.vehicale_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicaleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
