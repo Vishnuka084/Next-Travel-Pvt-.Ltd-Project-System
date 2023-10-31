b8bf7e8fbd644b41dcf0a5621f1a031f67efcf74
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 00:58:14 +0530

    Updated Contnet

--- User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
@@ -6,28 +6,5 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    @PostMapping()
-    public String saveUser(){
-        return "save User";
-    }
 
-    @GetMapping
-    public String findUser(){
-        return "find User";
-    }
-
-/*    @PostMapping(path = "/save")
-    public String saveUser(){
-        return "save User";
-    }
-
-    @DeleteMapping(path = "/delete")
-    public String deleteUser(){
-        return "Delete User";
-    }
-
-    @PutMapping(path = "/update")
-    public String updateUser(){
-        return "Update user";
-    }*/
 }
