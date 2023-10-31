a49404ff1b43e640c9e06aff14d86774bed829be
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 00:58:40 +0530

    Updated Contnet

--- User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
@@ -1,9 +1,12 @@
 package lk.ijse.vishnuka084.user_service.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("api/v1/user")
+@CrossOrigin
+@RequiredArgsConstructor
 public class UserController {
 
 
