c79a44b16a3bae11fb3107f9c53c14e227a563c7
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-31 22:20:37 +0530

    Updated Content

---
+++ Guide Service/src/test/java/lk/ijse/vishnuka084/guide_service/GuideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.vishnuka084.guide_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
