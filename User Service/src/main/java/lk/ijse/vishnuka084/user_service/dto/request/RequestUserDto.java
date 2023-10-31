787ba049f4bc76a2bbc770eaa848d0974f756f6a
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-31 23:22:57 +0530

    updated Content

---
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/dto/request/RequestUserDto.java
@@ -0,0 +1,22 @@
+package lk.ijse.vishnuka084.user_service.dto.request;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class RequestUserDto {
+    private String name;
+
+    private String address;
+
+    private String contact;
+
+    private String Bod;
+
+
+
+}
