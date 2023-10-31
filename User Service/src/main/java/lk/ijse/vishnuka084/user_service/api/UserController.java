4ea4d5a25d6194c50db74725cdb786d81066f472
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-31 23:02:15 +0530

    updated Content

--- User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/api/UserController.java
@@ -1,2 +1,23 @@
-package lk.ijse.vishnuka084.user_service.api;public class UserController {
+package lk.ijse.vishnuka084.user_service.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+    @PostMapping(path = "/save")
+    public String saveUser(){
+        return "save User";
+    }
+
+    @DeleteMapping(path = "/delete")
+    public String deleteUser(){
+        return "Delete User";
+    }
+
+    @PutMapping(path = "/update")
+    public String updateUser(){
+        return "Update user";
+    }
 }
