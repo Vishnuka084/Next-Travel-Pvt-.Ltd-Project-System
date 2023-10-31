f4391194b608bc0032023401977e097f7f95aa20
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 01:17:35 +0530

    add Exception package

---
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/feign/VehicleFeign.java
@@ -0,0 +1,4 @@
+package lk.ijse.vishnuka084.user_service.feign;
+
+public interface VehicleFeign {
+}
---
+++ User Service/src/main/java/lk/ijse/vishnuka084/user_service/service/FinancialService.java
@@ -0,0 +1,8 @@
+package lk.ijse.vishnuka084.user_service.service;
+
+import lk.dhanushkaTa.userservice.dto.FinancialDTO;
+
+public interface FinancialService {
+
+    FinancialDTO getIncomeDetails(String date);
+}
