62c7f0a4b1c6ea2be440ebcb6fd63b010133a1b5
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 17:31:57 +0530

    Updated Content

---
+++ Guide Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/guideservice/exception/NotFoundException.java
@@ -0,0 +1,15 @@
+package lk.NextTravelPvt.Ltd.vishnuka084.guideservice.exception;
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author  : Vishnuka Yahan
+ * @data    : 11/1/2023
+ * @project : Next TravelPvt. Ltd
+ *
+ */
+public class NotFoundException extends Exception{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ Guide Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/guideservice/exception/ProcessingException.java
@@ -0,0 +1,23 @@
+package lk.NextTravelPvt.Ltd.vishnuka084.guideservice.exception;
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author  : Vishnuka Yahan
+ * @data    : 11/1/2023
+ * @project : Next TravelPvt. Ltd
+ *
+ */
+public class ProcessingException extends Exception{
+
+    public ProcessingException(String message) {
+        super(message);
+    }
+
+    public ProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProcessingException(Throwable cause) {
+        super(cause);
+    }
+}
