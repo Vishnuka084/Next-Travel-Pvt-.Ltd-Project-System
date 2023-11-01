2c11a6f251eaaa8374cb42f05619e8c4dda9cf3c
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 11:58:07 +0530

    add exception pacakges

---
+++ Hotel Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/hotelservice/exception/DuplicateException.java
@@ -0,0 +1,8 @@
+package lk.NextTravelPvt.Ltd.vishnuka084.hotelservice.exception;
+
+public class DuplicateException extends Exception{
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
