3547aa6001927664c57783d0ff1084183ea5a463
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 17:20:28 +0530

    add guide application properties

--- Guide Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/guideservice/GuideServiceApplication.java
+++ Guide Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/guideservice/GuideServiceApplication.java
@@ -1,4 +1,4 @@
-package lk.ijse.vishnuka084.guide_service;
+package lk.NextTravelPvt.Ltd.vishnuka084.guideservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GuideServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GuideServiceApplication.class, args);
+	}
 
 }
