253fde88c527126ac384fd8b1a5628621dd29a9e
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-01 12:00:39 +0530

    Updated Hotel repository class

--- Hotel Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/hotelservice/repository/HotelRepository.java
+++ Hotel Service/src/main/java/lk/NextTravelPvt.Ltd/vishnuka084/hotelservice/repository/HotelRepository.java
@@ -1,4 +1,20 @@
 package lk.NextTravelPvt.Ltd.vishnuka084.hotelservice.repository;
 
-public class HotelRepository {
+import lk.NextTravelPvt.Ltd.vishnuka084.hotelservice.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel,String> {
+
+    List<Hotel> findByHotelNameIsLikeIgnoreCaseOrderByHotelCategoryAsc(String hotelName);
+    List<Hotel> findByHotelNameStartingWithOrderByHotelCategoryAsc(String hotelName);
+    //    Hotel findByHotelName(String hotelName);
+    Hotel findByHotelNameIgnoreCase(String hotelName);
+
+    @Query(value = "select h from Hotel h order by h.hotelId desc")
+    List<Hotel> getLastHotelId();
 }
