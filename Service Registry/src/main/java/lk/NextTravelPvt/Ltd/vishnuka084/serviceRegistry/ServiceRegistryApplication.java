ca7874a241c6af0a91ca76b8c7c6c90cc622c210
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-02 10:31:11 +0530

    add ServiceRegistryApplication java class

---
+++ Service Registry/src/main/java/lk/NextTravelPvt/Ltd/vishnuka084/serviceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,11 @@
+package lk.NextTravelPvt.Ltd.vishnuka084.serviceRegistry;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/2/2023
+ * @project : Next TravelPvt. Ltd
+ */
+public class ServiceRegistryApplication {
+}
