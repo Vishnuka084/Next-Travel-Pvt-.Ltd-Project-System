b9ba9b75cfefe4f5d5f08865997ac5ea15e5f6f9
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-02 10:40:20 +0530

    update  api gateway module

--- Api Gateway/src/main/java/lk/NextTravelPvt/Ltd/vishnuka084/ApiGatewayApplication.java
+++ Api Gateway/src/main/java/lk/NextTravelPvt/Ltd/vishnuka084/ApiGatewayApplication.java
@@ -1,11 +1,15 @@
-package com.example.apigateway;
+package lk.NextTravelPvt.Ltd.vishnuka084;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/2/2023
+ * @project : Next TravelPvt. Ltd
+ */
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
