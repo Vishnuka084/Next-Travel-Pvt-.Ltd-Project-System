4738ae95d11e11c4a9cb76f6232d085d280241d4
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-06 15:15:26 +0530

    Updated Maven

--- Vehicle Service/src/main/java/lk/NextTravelPvt/Ltd/vishnuka084/vehicleservice/advisor/AppWideExceptionHandler.java
+++ Vehicle Service/src/main/java/lk/NextTravelPvt/Ltd/vishnuka084/vehicleservice/advisor/AppWideExceptionHandler.java
@@ -3,6 +3,7 @@ package lk.NextTravelPvt.Ltd.vishnuka084.vehicleservice.advisor;
 import lk.NextTravelPvt.Ltd.vishnuka084.vehicleservice.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
@@ -13,13 +14,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @project : Next TravelPvt. Ltd
  *
  */
-@RestControllerAdvice
-@CrossOrigin
-public class ExceptionHandler {
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
-    public ResponseUtil sendResponse(Exception e){
-        return new ResponseUtil(500,e.getMessage(),null);
+@CrossOrigin
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e) {
+        return new ResponseUtil(500, e.getMessage(), null);
     }
+
 }
