0038b9240e2246eba93f87a4e814240cd9706118
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-14 19:13:50 +0530

    Updated Content

--- Vehicle Service/src/main/java/lk/ijse/travelPackage/VehicleService/VehicleServiceApplication.java
+++ Vehicle Service/src/main/java/lk/ijse/travelPackage/VehicleService/VehicleServiceApplication.java
@@ -2,7 +2,14 @@ package lk.ijse.travelPackage.VehicleService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author  : Vishnuka Yahan
+ * @data    : 11/01/2023
+ * @project : Next TravelPvt. Ltd
+ *
+ */
 @SpringBootApplication
 public class VehicleServiceApplication {
 
